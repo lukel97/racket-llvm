19b13448665a6d301c8f66a3b67bef09eb6e7006
luke <luke_lau@icloud.com>
2018-03-16 21:28:17 -0400

    Export functions, add a test and example

--- racket-llvm.rkt
+++ racket-llvm.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang racket
 
 (require ffi/unsafe
          ffi/unsafe/define)
@@ -16,63 +16,45 @@
 
 (define-llvm llvm-module-create-with-name (_fun _string -> _LLVMModuleRef)
              #:c-id LLVMModuleCreateWithName)
+(provide llvm-module-create-with-name)
 
 (define-llvm llvm-function-type (_fun  _LLVMTypeRef ; return type
                                        (param-types : (_list i _LLVMTypeRef)) ; param types
                                        (_int = (length param-types)) ; num params
-                                       _int  ; variadic?
+                                       _bool ; variadic?
                                        -> _LLVMTypeRef)
              #:c-id LLVMFunctionType)
+(provide llvm-function-type)
 
 (define-llvm llvm-add-function (_fun _LLVMModuleRef
                                      _string
                                      _LLVMTypeRef
                                      -> _LLVMValueRef)
              #:c-id LLVMAddFunction)
+(provide llvm-add-function)
 
-
-(define mod (llvm-module-create-with-name "testModule"))
-
-#| C code taken from
-   https://pauladamsmith.com/blog/2015/01/how-to-get-started-with-llvm-c-api.html |#
-
-#|  LLVMTypeRef param_types[] = { LLVMInt32Type(), LLVMInt32Type() };
-    LLVMTypeRef ret_type = LLVMFunctionType(LLVMInt32Type(), param_types, 2, 0);
-    LLVMValueRef sum = LLVMAddFunction(mod, "sum", ret_type); |#
 
 (define-llvm llvm-int32-type (_fun -> _LLVMTypeRef)
              #:c-id LLVMInt32Type)
-
-(define return-type (llvm-function-type (llvm-int32-type) (list (llvm-int32-type) (llvm-int32-type)) 0))
-(define sum (llvm-add-function mod "sum" return-type))
-
-#| LLVMBasicBlockRef entry = LLVMAppendBasicBlock(sum, "entry"); |#
-
+(provide llvm-int32-type)
 
 (define-llvm llvm-append-basic-block (_fun _LLVMValueRef
                                            _string
                                            -> _LLVMBasicBlockRef)
              #:c-id LLVMAppendBasicBlock)
-
-(define entry (llvm-append-basic-block sum "entry"))
-
-#|  LLVMBuilderRef builder = LLVMCreateBuilder();
-    LLVMPositionBuilderAtEnd(builder, entry); |#
+(provide llvm-append-basic-block)
 
 (define-llvm llvm-create-builder (_fun -> _LLVMBuilderRef) #:c-id LLVMCreateBuilder)
+(provide llvm-create-builder)
 
 (define-llvm llvm-position-builder-at-end (_fun _LLVMBuilderRef
                                                 _LLVMBasicBlockRef
                                                 -> _void)
              #:c-id LLVMPositionBuilderAtEnd)
-
-(define builder (llvm-create-builder))
-(llvm-position-builder-at-end builder entry)
-
-#|  LLVMValueRef tmp = LLVMBuildAdd(builder, LLVMGetParam(sum, 0), LLVMGetParam(sum, 1), "tmp");
-    LLVMBuildRet(builder, tmp); |#
+(provide llvm-position-builder-at-end)
 
 (define-llvm llvm-get-param (_fun _LLVMValueRef _int -> _LLVMValueRef) #:c-id LLVMGetParam)
+(provide llvm-get-param)
 
 (define-llvm llvm-build-add (_fun _LLVMBuilderRef
                                   _LLVMValueRef
@@ -80,17 +62,11 @@
                                   _string
                                   -> _LLVMValueRef)
              #:c-id LLVMBuildAdd)
+(provide llvm-build-add)
 
 (define-llvm llvm-build-ret (_fun _LLVMBuilderRef _LLVMValueRef -> _void)
              #:c-id LLVMBuildRet)
-
-
-(define tmp (llvm-build-add builder (llvm-get-param sum 0) (llvm-get-param sum 1) "tmp"))
-(llvm-build-ret builder tmp)
-
-#|  char *error = NULL;
-    LLVMVerifyModule(mod, LLVMAbortProcessAction, &error);
-    LLVMDisposeMessage(error); |#
+(provide llvm-build-ret)
 
 (define _LLVMVerifierFailureAction
   (_enum '(llvm-abort-process-action
@@ -99,6 +75,7 @@
 
 (define-llvm llvm-dispose-message (_fun (_ptr i _string) -> _void)
              #:c-id LLVMDisposeMessage)
+(provide llvm-dispose-message)
 
 (define-llvm llvm-verify-module (_fun _LLVMModuleRef
                                       (_LLVMVerifierFailureAction = 'llvm-return-status-action)
@@ -107,29 +84,18 @@
                                       ; TODO: should call llvm-dispose-message
                                       -> (when failure (error err)))
              #:c-id LLVMVerifyModule)
-
-(llvm-verify-module mod)
-
-#|  LLVMExecutionEngineRef engine;
-    error = NULL;
-    LLVMLinkInMCJIT();
-    LLVMInitializeNativeTarget();
-    if (LLVMCreateExecutionEngineForModule(&engine, mod, &error) != 0) {
-        fprintf(stderr, "failed to create execution engine\n");
-        abort();
-    }
-    if (error) {
-        fprintf(stderr, "error: %s\n", error);
-        LLVMDisposeMessage(error);
-        exit(EXIT_FAILURE);
-    } |#
+(provide llvm-verify-module)
 
 (define _LLVMExecutionEngineRef (_cpointer 'LLVMOpaqueExecutionEngine))
 
 (define-llvm llvm-link-in-mcjit (_fun -> _void) #:c-id LLVMLinkInMCJIT)
+(provide llvm-link-in-mcjit)
+
+(llvm-link-in-mcjit)
 
 ;TODO: figure out a way of replacing X86 with the current arch
 (define-llvm llvm-initialize-native-target (_fun -> _bool) #:c-id LLVMInitializeX86Target)
+(provide llvm-initialize-native-target)
 
 (define-llvm llvm-create-execution-engine-for-module
              (_fun (eng : (_ptr o _LLVMExecutionEngineRef))
@@ -141,35 +107,18 @@
                         [(not (= 0 result)) (error "Failed to create execution engine")]
                         [else eng]))
              #:c-id LLVMCreateExecutionEngineForModule)
+(provide llvm-create-execution-engine-for-module)
 
-(define eng (llvm-create-execution-engine-for-module mod))
-
-#|  if (argc < 3) {
-        fprintf(stderr, "usage: %s x y\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    long long x = strtoll(argv[1], NULL, 10);
-    long long y = strtoll(argv[2], NULL, 10); |#
-
-(define-values (x y) (values (read) (read)))
-
-#|  LLVMGenericValueRef args[] = {
-      LLVMCreateGenericValueOfInt(LLVMInt32Type(), x, 0),
-      LLVMCreateGenericValueOfInt(LLVMInt32Type(), y, 0)
-  }; |#
 
 (define _LLVMGenericValueRef (_cpointer 'LLVMOpaqueGenericValue))
 
 (define-llvm llvm-create-generic-value-of-int (_fun _LLVMTypeRef
                                                     _int ; value
-                                                    _int ; signed?
+                                                    _bool ; signed?
                                                     -> _LLVMGenericValueRef)
              #:c-id LLVMCreateGenericValueOfInt)
+(provide llvm-create-generic-value-of-int)
 
-(define args (list (llvm-create-generic-value-of-int (llvm-int32-type) x 0)
-                   (llvm-create-generic-value-of-int (llvm-int32-type) y 0)))
-
-#|  LLVMGenericValueRef res = LLVMRunFunction(engine, sum, 2, args); |#
 
 (define-llvm llvm-run-function (_fun _LLVMExecutionEngineRef
                                      _LLVMValueRef
@@ -177,12 +126,47 @@
                                      (args : (_list i _LLVMGenericValueRef))
                                      -> _LLVMGenericValueRef)
              #:c-id LLVMRunFunction)
-
-(define res (llvm-run-function eng sum args))
-
-#| printf("%d\n", (int)LLVMGenericValueToInt(res, 0)); |#
+(provide llvm-run-function)
 
 (define-llvm llvm-generic-value-to-int (_fun _LLVMGenericValueRef _bool -> _llong)
              #:c-id LLVMGenericValueToInt)
+(provide llvm-generic-value-to-int)
 
-(println (llvm-generic-value-to-int res #f))
+(define-llvm llvm-module-to-string (_fun _LLVMModuleRef -> _string)
+             #:c-id LLVMPrintModuleToString)
+(provide llvm-module-to-string)
+
+(module+ test
+  (require rackunit)
+  
+  (let*
+    ([mod (llvm-module-create-with-name "testModule")]
+     [return-type (llvm-function-type (llvm-int32-type)
+                                      (list (llvm-int32-type)
+                                            (llvm-int32-type))
+                                      #f)]
+     [sum (llvm-add-function mod "sum" return-type)]
+
+     [entry (llvm-append-basic-block sum "entry")]
+
+     [builder (llvm-create-builder)])
+
+    (begin
+      (llvm-position-builder-at-end builder entry)
+      (let ([tmp (llvm-build-add builder
+                                 (llvm-get-param sum 0)
+                                 (llvm-get-param sum 1)
+                                 "tmp")])
+        (begin
+          (llvm-build-ret builder tmp)
+
+          (llvm-verify-module mod)
+
+          (let* 
+            ([eng (llvm-create-execution-engine-for-module mod)]
+             [args (list (llvm-create-generic-value-of-int (llvm-int32-type) 20 #f)
+                         (llvm-create-generic-value-of-int (llvm-int32-type) 22 #f))]
+             [res (llvm-run-function eng sum args)])
+            (check-equal? 42 (llvm-generic-value-to-int res #f))))))))
+
+;TODO: test error thrown when llvm-verify-module fails

a7a854c8450edbb3595ff893e83b6d6599fbd33b
luke <luke_lau@icloud.com>
2018-03-17 02:05:56 -0400

    Add info.rkt and travis tweaks

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "racket-llvm")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "LLVM bindings for Racket")
+(define version "0.0")
+(define pkg-authors '(Luke Lau))

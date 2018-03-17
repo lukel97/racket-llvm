ab8580ebd41bef155c0f7b29e435a8ff704c2b85
luke <luke_lau@icloud.com>
2018-03-17 00:57:22 -0400

    Travis

--- racket-llvm.rkt
+++ racket-llvm.rkt
@@ -10,7 +10,7 @@
 (println (get-llvm-lib-dir))
 
 (define-ffi-definer define-llvm (ffi-lib "libLLVM"
-                                         ;'("6" #f)
+                                         '("6" #f)
                                          ;TODO: Add more search directories
                                          #:get-lib-dirs get-llvm-lib-dir))
 
